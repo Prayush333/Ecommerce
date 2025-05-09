fc20f9ace8d296ce289fdfd96261c6fba4e5d7b3
Prayush Neupane <pyprayushneupane@gmail.com>
2025-05-09 22:15:28 +0545

    category+customer+product models

--- ecom_app/models.py
+++ ecom_app/models.py
@@ -8,7 +8,21 @@ class Catagory(models.Model):
         return self.name
     
 class Customer(models.Model):
-    pass
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=100)
 
-
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
     
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, decimal_places=3, max_digits=8)
+    category = models.ForeignKey(Catagory,on_delete=models.CASCADE, default =1 )
+    
+
+class Order(models.Model):
+    pass
