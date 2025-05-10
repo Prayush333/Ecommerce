b2fb3a76fc9b6c6cc01aebc276664ade0512d0f4
Prayush Neupane <pyprayushneupane@gmail.com>
2025-05-10 11:01:27 +0545

    product+order model

--- ecom_app/models.py
+++ ecom_app/models.py
@@ -22,7 +22,21 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(default=0, decimal_places=3, max_digits=8)
     category = models.ForeignKey(Catagory,on_delete=models.CASCADE, default =1 )
+    description = models.CharField(max_length=300, default ='',blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/product/')
     
+    def __str__(self):
+        return self.name
+
 
 class Order(models.Model):
-    pass
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100, default='' , blank=True, null=True)
+    phone = models.CharField(max_length=15)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField()
+
+    def __str__(self):
+        return self.product
